03a8677850827a171cf853865721d09a75e121ee
marcolteodoro <marco.lteodoro@icloud.com>
2026-01-29 10:59:21 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = "agenda-demandas-v1";
+const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
